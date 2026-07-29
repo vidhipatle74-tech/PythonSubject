3b2c94c8f468f25c84d95497b0b7165d4b9e4e57
vidhi patle <vidhipatle74@gmail.com>
2026-07-30 04:46:06 +0530

    clone

---
+++ conditional statement.py
@@ -0,0 +1,35 @@
+
+
+
+                          #CONDITIONAL STATEMENT:
+
+#It controls the flow of the program execution  based on the given conditions.
+
+
+ #TYPES:
+
+#(1)--> simple if / if condition
+#(2)--> if-else condition
+#(3)--> elif condition
+#(4)--> nested if-else condition
+
+
+#(1)--> simple if / if condition:
+
+#if the given condition is working it will excute inside the true state block(TSB).
+#if the given condition is not satisfied it will show blank space (FSB).
+
+
+#FLOWCHART:
+
+#                              START
+#                               |
+#             true              |               false
+#           --------------------------------------------
+#           |                                           |
+#      It will execute                    It will skip the code and
+#       true state block                  it will show blank space 
+
+
+#(2)--> if-else condition:
+
