e6947f459ab418dc414b26df5806f91b4ac4ad6a
vidhi patle <vidhipatle74@gmail.com>
2026-07-31 21:16:17 +0530

    add in python

---
+++ elif condition.py
@@ -0,0 +1,57 @@
+#ELIF condition:
+
+#when we have  multiple conditions  we can use elif condition.
+
+#in elif condition we can use ele block but else block is optional.
+
+#in elif condition we can use multiple elif block based on the condition
+
+#SYNTAX:
+#        if condition :
+#       <--> statement    --->TSB
+#        |
+#        elif condition:
+#        <--> statement   --->TSB
+#        |
+#        elif condition:
+#       <--> statement    -->TSB
+#        |
+#        elif condition:
+#       <--> statement    --->TSB
+#        |
+#        else:
+#             condition    --->FSB
+
+#where,
+#       ==else block is optional==
+
+
+       '''FLOWCHART:
+
+#                                     Start
+#                                       |
+#                                       |
+#                                   Condition
+#                                       |
+#               -------------------------------------------------
+#               |                                                |
+#             TRUE                                             FALSE
+#               |                                                |
+#        block will executed                                condition 2
+#                                                                |
+#                                               --------------------------------------
+#                                               |                                     |
+#                                            TRUE                                   FALSE
+#                                               |                                     |
+#                                   block will execute                             condition 3
+#                                                                                      |
+#                                                                                      |
+#                                                                -------------------------------------------
+#                                                                |                                          |
+#                                                             TRUE                                        FALSE
+#                                                                |                                          |
+#                                                            block will execute                         blank  space
+'''
+
+       
+       
