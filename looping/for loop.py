702ca93627ced3acce688a4f68e8d580510f30f0
vidhi patle <vidhipatle74@gmail.com>
2026-08-05 06:48:19 +0530

    add

---
+++ looping/for loop.py
@@ -0,0 +1,30 @@
+
+# wap the program to print number 1 to 5.
+'''a=[1,2,3,4,5]
+for i in a:
+    print(a)
+'''
+
+#wap to print the even  number
+'''  
+a=[1,2,3,4,5,6,7,8,9]
+for i in a:
+    if i%2==0:
+        print(i)
+'''
+#wap to convert all list element in uppercase.
+
+'''a=["vidhi","bhakti","apeksha","jyoti","kamlesh"]
+
+for i in a:
+    print(i.upper())
+'''
+a=["vidhi","bhakti","apeksha","jyoti","kamlesh"]
+for i in a :
+    print("-----",i[0],"----",i[-1])
+
+
+
+
+    
+
