7c0187fbd0e30400bcdd31f1c2b61c495a6cb2b9
vidhi patle <vidhipatle74@gmail.com>
2026-08-12 02:32:48 +0530

    add

---
+++ looping/zip_longest for loop.py
@@ -0,0 +1,17 @@
+#ZIP_LONGEST FOR LOOP:
+#                   --> It is a inbuilt function.
+#                   --> zip longest is present [from itertools import zip_longest].
+#                   --> if specified position is not match it will replace with none element.
+#                   --> here data will not be lost.
+#                   --> if you dont want that then you can use [fillvalue=value]
+#                   --> it match position to position.
+#                   --> fill value always accepts one element if you want to pass more than one
+#                       element.
+#                   then you can use syntax like.
+
+#SYNATX:--> from itertools import zip_longest
+#           (zip_longest(iterable1,iterable2,......,fillvalue=value))
+
+
+#LOOPING SYNTAX:-->
+#                   for variable in zip_longest(iterable1,iterable2,....,fillvalue=value)
