1242620c7a313f0b603a67922f8fd0cfe01a9463
vidhi patle <vidhipatle74@gmail.com>
2026-08-18 02:22:02 +0530

    add

---
+++ DATATYPES/boolean datatype.py
@@ -0,0 +1,22 @@
+
+#BOOLEAN DATATYPE:
+#                -->It is a individual datatype which consists of two value in it.
+
+#       #True
+#       #False
+
+#       --> True and False are the special keyword.
+#       --> we can assign this keywords as a value to a variable.
+#       --> here "True" is non-default value
+#                         &
+#       --> "False" is the default value.
+
+#bool()--> it is a inbuilt function
+#      --> it displays a result in the form of true and false.
+
+#boolean datatype--> it is a individual datatype
+#                --> it will contain value like  true and false.
+
+
+a=4+5j
+bool(a)
