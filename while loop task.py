7e5eb67bf6f04635b271b6f877294d24b14b8ae7
vidhi patle <vidhipatle74@gmail.com>
2026-08-17 12:21:19 +0530

    add

--- while loop task.py
+++ while loop task.py
@@ -239,7 +239,7 @@ print("Sum of digits:", sum)
 '''
 
 #WAP to check whether the number is perfect or not.
-
+'''
 n = int(input("Enter a number: "))
 
 i = 1
@@ -254,3 +254,133 @@ if sum == n:
     print("Perfect number")
 else:
     print("Not a perfect number")
+
+o/p-->
+       Enter a number: 5
+       Not a perfect number
+'''
+#wap to login to phonepay to enter the correct otp
+'''
+correct_otp = "1234"
+attempts = 0
+max_attempts = 3
+
+while attempts < max_attempts:
+    otp = input("Enter OTP: ")
+
+    if otp == correct_otp:
+        print("Login successful")
+        break
+    else:
+        attempts += 1
+        print("Incorrect OTP")
+
+if attempts == max_attempts:
+    print("Too many wrong attempts. Login failed.")
+
+#o/p-->
+    Enter OTP: 1234
+    Login successful
+'''
+#wap to extract all the even integer present in the  tuple at odd index.
+'''
+t = (10, 15, 22, 35, 40, 51, 60, 75)
+i = 1
+
+while i <=len(t):
+    if t[i] % 2 == 0:                #remaining ans is showing blank space
+        print(t[i])
+    i += 2
+'''
+
+#WAP to remove duplicates from a list without converting into set.
+'''
+l = [10, 20, 10, 30, 20, 40, 30]
+
+new = []
+i = 0
+
+while i < len(l):
+    if l[i] not in new:
+        new.append(l[i])
+    i += 1
+
+print(new)
+
+#o/p-->
+       [10, 20, 30, 40]
+'''
+
+#WAP to find the sum of all the odd numbers between the given range.
+'''
+start = int(input("Enter starting number: "))
+end = int(input("Enter ending number: "))
+
+sum = 0
+
+while start <= end:
+    if start % 2 != 0:
+        sum = sum + start
+    start += 1
+
+print("Sum of odd numbers =", sum)
+
+o/p-->
+      Enter starting number: 3
+      Enter ending number: 5
+      Sum of odd numbers = 8
+'''
+#WAP to find the greatest number in a given list of integers.
+'''
+l = [10, 25, 7, 45, 18, 32]
+
+i = 0
+greatest = l[0]
+
+while i < len(l):
+    if l[i] > greatest:
+        greatest = l[i]
+    i += 1
+
+print("Greatest number =", greatest)
+
+#o/p--> Greatest number = 45
+'''
+#WAP to find the sum of cube of a number in a string.
+'''
+s = "12345"
+
+i = 0
+sum = 0
+
+while i < len(s):
+    if s[i].isdigit():
+        n = int(s[i])
+        sum = sum + n ** 3
+    i += 1
+
+print("Sum of cubes =", sum)
+
+#o/p--> Sum of cubes = 225
+'''
+
+#WAP to check whether the number is Armstrong or not.
+'''
+n = int(input("Enter a number: "))
+
+temp = n
+sum = 0
+
+while n > 0:
+    digit = n % 10
+    sum = sum + digit ** 3
+    n = n // 10
+
+if sum == temp:
+    print("Armstrong number")
+else:
+    print("Not an Armstrong number")
+
+#o/p--> Enter a number: 153
+        Armstrong number
+'''
