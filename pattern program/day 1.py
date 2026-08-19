3e674ccafcae8c3234714b52d00655ee766f7367
vidhi patle <vidhipatle74@gmail.com>
2026-08-20 03:47:48 +0530

    add

--- pattern program/day 1.py
+++ pattern program/day 1.py
@@ -1,3 +1,5 @@
+#(1)
+'''
 rows=int(input("enter the rows:"))
 columns=int(input("enter the columns:"))
 for i in range(rows):
@@ -7,3 +9,18 @@ for i in range(rows):
         else:
             print('*',end=' ')
     print()
+
+#o/p-->
+        enter the rows:7
+        enter the columns:8
+        * * * * * * * * 
+        *  *  *  *  
+        * * * * * * * * 
+        *  *  *  *  
+        * * * * * * * * 
+        *  *  *  *  
+        * * * * * * * * 
+'''
+#(2)
+
+rows=int(input())
---
+++ statement.py
@@ -0,0 +1,59 @@
+#Flow control statements:
+'''
+#-->*Controlling the flow of program execution is called flow control statements.
+
+#-->*flow control statement is classified into 2 types,
+
+#-->1.conditional flow control statement
+'''
+#2.loop flow control statement
+
+
+
+#1.conditional flow control statement:
+
+********************************
+
+#*Controlling a flow of program execution based on condition is called a conditional flow control statement.
+
+#*conditional statement is classified into 4 types,
+
+#1.simple if
+#2.if-else
+#3.elif
+#4.nested if-else
+
+#1.simple if:
+#************
+#*if cond become True "if block/TSB" will get executed else it will just go with flow of program execution. 
+
+#syntax:
+#*******
+#if cond:
+#   stmt1 } 
+#   stmt2 }---> TSB/if block
+#   stmt3 }    
+
+  :
+
+"""
+drawback of simple-if:
+**********************
+*if cond is False we don't have any specific block of code to execute, to over this we go "if-else"
+
+
+
+2. if-else:
+***********
+*if cond become "True" "if block/TSB" will get executed else if cond become "False" "else block/FSB" will execute.
+ 
+syntax:
+*******
+if cond:
+   stmt1 } 
+   stmt2 }---> TSB/if block
+   stmt3 }   
+else: 
+    stmt5 }---> FSB/else block
+    stmt6 }
+"""
